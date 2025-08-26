0063f6cd1bd893dcd3e17cc46f833a7f08fb59b7
Thomas Woods <thomaswoods@canusa-apc.com>
2025-08-26 14:55:52 -0400

    Updates

---
+++ precache-manifest.5d3113f795fbc8fb5a59315f8ed9cf54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ec604c7e833cf8e0b9f",
+    "url": "/thomaswoodsportfolio/static/js/main.1ec604c7.chunk.js"
+  },
+  {
+    "revision": "21fd976cd1ad4806e25d",
+    "url": "/thomaswoodsportfolio/static/js/runtime~main.21fd976c.js"
+  },
+  {
+    "revision": "f53ffcb5180cfa0add39",
+    "url": "/thomaswoodsportfolio/static/css/2.5214719a.chunk.css"
+  },
+  {
+    "revision": "f53ffcb5180cfa0add39",
+    "url": "/thomaswoodsportfolio/static/js/2.f53ffcb5.chunk.js"
+  },
+  {
+    "revision": "a88a8d12888612c909d7d01a83270447",
+    "url": "/thomaswoodsportfolio/index.html"
+  }
+];
